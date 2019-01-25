186ce82701f3dfb00c2226313694e4f2ba659d33
Aidan Clyens <aidanc@rogers.com>
2019-01-25 14:34:52 -0500

    Added Google Tests to Vector library

---
+++ Vector/src/tests/VectorTest.cpp
@@ -0,0 +1,46 @@
+#include <gtest/gtest.h>
+#include "../Vector.h"
+
+class VectorTest : public testing::Test {
+    public:
+        VectorTest() {}
+
+        virtual ~VectorTest() {}
+};
+
+TEST(VectorTest, Initialize) {
+    vector<int> vec;
+    vector<int> vec2(32);
+
+    ASSERT_EQ(0, vec.size());
+    ASSERT_EQ(1, vec.empty());
+    ASSERT_EQ(16, vec.capacity());
+    
+    ASSERT_EQ(0, vec2.size());
+    ASSERT_EQ(1, vec2.empty());
+    ASSERT_EQ(32, vec2.capacity());
+}
+
+TEST(VectorTest, PushBack) {
+    vector<int> vec;
+
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+    vec.push_back(4);
+    vec.push_back(5);
+    vec.push_back(6);
+    vec.push_back(7);
+
+    ASSERT_EQ(7, vec.size());
+    ASSERT_EQ(0, vec.empty());
+    ASSERT_EQ(16, vec.capacity());
+    ASSERT_EQ(1, vec.front());
+    ASSERT_EQ(7, vec.back());
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
