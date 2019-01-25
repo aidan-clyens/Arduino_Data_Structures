7ee2a387497200820a0ae84dae801526c9bbf35e
Aidan Clyens <aidanc@rogers.com>
2019-01-25 15:23:18 -0500

    Increase Vector test coverage

--- Vector/src/tests/VectorTest.cpp
+++ Vector/src/tests/VectorTest.cpp
@@ -37,6 +37,64 @@ TEST(VectorTest, PushBack) {
     ASSERT_EQ(16, vec.capacity());
     ASSERT_EQ(1, vec.front());
     ASSERT_EQ(7, vec.back());
+    ASSERT_EQ(3, vec.at(2));
+    ASSERT_EQ(3, vec[2]);
+    ASSERT_EQ(5, vec.at(4));
+    ASSERT_EQ(5, vec[4]);
+}
+
+TEST(VectorTest, PopBack) {
+    vector<int> vec;
+
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+    vec.push_back(4);
+    vec.push_back(5);
+    vec.push_back(6);
+    vec.push_back(7);
+    
+    vec.pop_back();
+    vec.pop_back();
+    vec.pop_back();
+
+    ASSERT_EQ(4, vec.size());
+    ASSERT_EQ(0, vec.empty());
+    ASSERT_EQ(16, vec.capacity());
+    ASSERT_EQ(1, vec.front());
+    ASSERT_EQ(4, vec.back());
+}
+
+TEST(VectorTest, IncreaseCapacity) {
+    vector<int> vec;
+    
+    for (int i = 0; i < 20; i++) {
+        vec.push_back(i);
+    }
+
+    ASSERT_EQ(20, vec.size());
+    ASSERT_EQ(0, vec.empty());
+    ASSERT_EQ(32, vec.capacity());
+    ASSERT_EQ(0, vec.front());
+    ASSERT_EQ(19, vec.back());
+}
+
+TEST(VectorTest, DecreaseCapacity) {
+    vector<int> vec(32);
+    
+    for (int i = 0; i < 25; i++) {
+        vec.push_back(i);
+    }
+
+    for (int i = 0; i < 18; i++) {
+        vec.pop_back();
+    }
+
+    ASSERT_EQ(7, vec.size());
+    ASSERT_EQ(0, vec.empty());
+    ASSERT_EQ(16, vec.capacity());
+    ASSERT_EQ(0, vec.front());
+    ASSERT_EQ(6, vec.back());
 }
 
 int main(int argc, char **argv) {
